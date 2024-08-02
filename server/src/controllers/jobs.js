6e4c2243ffde9ca9b02f0b3da5e9547e9afd47ac
Luka <jovanovicluka276@outlook.com>
2024-08-02 12:23:27 +0300

    add pagination

--- server/src/controllers/jobs.js
+++ server/src/controllers/jobs.js
@@ -43,8 +43,18 @@ const getAllJobs = async (req, res) => {
             break;
     }
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
     let jobs = await result;
-    res.status(StatusCodes.OK).json({ jobs });
+
+    const totalJobs = await Job.countDocuments(queryObject);
+    const numOfPages = Math.ceil(totalJobs / limit);
+
+    res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const getJob = async (req, res) => {
