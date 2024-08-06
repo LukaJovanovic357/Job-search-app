bdb527a41e28ec3410a883a670eec0bd02fe6767
Luka <jovanovicluka276@outlook.com>
2024-08-06 09:22:19 +0300

    adjust tsconfig

--- server/src/tests/app.test.ts
+++ server/src/tests/app.test.ts
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest';
 import app from '../app';
 
 describe('GET /', () => {
-    it.skip('should return 200 OK', async () => {
+    it('should return 200 OK', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
     });
